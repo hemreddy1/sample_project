06526e77a7f57664ff8aa0fa93fa42b6b8d90890
hemreddy1 <varikutihemanthreddy412@gmail.com>
2024-07-26 11:52:59 +0530

    Changes made in demo.java update m1()

--- demo.java
+++ demo.java
@@ -1,13 +1,18 @@
-public class Main {
+class FibonacciExample2{  
+ static int n1=0,n2=1,n3=0;    
+ static void printFibonacci(int count){    
+    if(count>0){    
+         n3 = n1 + n2;    
+         n1 = n2;    
+         n2 = n3;    
+         System.out.print(" "+n3);   
+         printFibonacci(count-1);    
+     }    
+ }    
+ public static void main(String args[]){    
+  int count=10;    
+  System.out.print(n1+" "+n2);//printing 0 and 1    
+  printFibonacci(count-2);//n-2 because 2 numbers are already printed   
+ }  
+}  
 
-  public static void main(String[] args) {
-    int rows = 5;
-
-    for (int i = 1; i <= rows; ++i) {
-      for (int j = 1; j <= i; ++j) {
-        System.out.print("* ");
-      }
-      System.out.println();
-    }
-  }
-}
